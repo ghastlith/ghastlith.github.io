dc5bf8afcc67773658d980b1a019409b78f505e0
ghastlith <72772611+ghastlith@users.noreply.github.com>
2024-05-03 16:36:22 -0300

    refactor: move images to own file

--- src/lib/assets/_index.js
+++ src/lib/assets/_index.js
@@ -1,6 +1,11 @@
-import background_large from './background-large.png';
-import background_small from './background-small.png';
-import white_rabbit_large from './white-rabbit-large.png';
-import white_rabbit_small from './white-rabbit-small.png';
+import background_large from './images/background-large.png';
+import background_small from './images/background-small.png';
+import white_rabbit_large from './images/white-rabbit-large.png';
+import white_rabbit_small from './images/white-rabbit-small.png';
 
-export { background_large, background_small, white_rabbit_large, white_rabbit_small };
+export {
+  background_large,
+  background_small,
+  white_rabbit_large,
+  white_rabbit_small
+};
