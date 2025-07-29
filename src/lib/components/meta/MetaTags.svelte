a193f320dba07d38b50d3f6b936a70e996ad7fbb
ghastlith <72772611+ghastlith@users.noreply.github.com>
2025-07-29 07:24:10 -0300

    feat: add purple color to mobile url bar

--- src/lib/components/meta/MetaTags.svelte
+++ src/lib/components/meta/MetaTags.svelte
@@ -5,7 +5,7 @@
     { name: "description", content: "Wonderland is waiting." },
     { name: "robots",      content: "nofollow, noarchive, noimageindex" },
     { name: "viewport",    content: "width=device-width, initial-scale=1.0, interactive-widget=resizes-content" },
-    { name: "theme-color", content: "#040404" },
+    { name: "theme-color", content: "#7E2C6F" },
   ];
 </script>
 
