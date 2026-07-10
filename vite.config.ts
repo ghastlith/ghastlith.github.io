c37ff607ea0b6d8bd58b6799889902e925f8807b
ghastlith <72772611+ghastlith@users.noreply.github.com>
2026-07-10 07:16:08 -0300

    refactor: bump package versions and move svelte config to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
+import adapter from "@sveltejs/adapter-static";
 import { sveltekit } from "@sveltejs/kit/vite";
 import Icons from "unplugin-icons/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
-    sveltekit(),
+    sveltekit({
+      adapter: adapter(),
+      alias: {
+        $components: "src/lib/components/index",
+        $functions: "src/lib/functions",
+        $images: "src/lib/images/index",
+        $styles: "src/lib/styles/styles.css",
+        $enums: "src/lib/enums",
+        $icons: "src/lib/icons",
+      },
+    }),
     Icons({ compiler: "svelte" }),
   ],
 });
